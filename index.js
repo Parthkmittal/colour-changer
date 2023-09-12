0cbaececfedb017928a736022fb8ce3a4295e957
Parthkmittal <cseitdeveloper@gmail.com>
2023-09-12 16:32:49 +0530

    colour changer

---
+++ index.js
@@ -0,0 +1,56 @@
+     
+
+
+function changeColor(color) {
+    document.body.style.background = color;
+}
+//  document.getElementById("bgcolor").style ="backgroundcolor : red"
+
+
+
+
+ 
+function red() 
+{
+    
+    document.getElementById("bgcolor").style = changeColor('red');
+         changeColor('red');
+         
+    
+}
+
+function blue() 
+{
+    changeColor('blue');
+    
+}
+
+function green() 
+{
+    changeColor('green');
+    
+}
+
+function yellow() 
+{
+    changeColor('yellow');
+    
+}
+
+function brown() 
+{
+    changeColor('brown');
+    
+}
+
+function pink() 
+{
+    changeColor('pink');
+    
+}
+
+function purpule() 
+{
+    changeColor('purple');
+    
+}
